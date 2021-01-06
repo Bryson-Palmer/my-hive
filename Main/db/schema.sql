3d01da5542e05b07fc2aae176f0825918b00a909
Bryson Palmer <brysonpalmer@gmail.com>
2021-01-05 18:49:43 -0800

    Build the schema file according to the README file.

---
+++ Main/db/schema.sql
@@ -0,0 +1,36 @@
+-- Drops the myHive_db if it already exists --
+DROP DATABASE IF EXISTS myHive_db;
+
+-- Create the database myHive_db and specified it for use.
+CREATE DATABASE myHive_db;
+
+USE myHive_db;
+
+-- Create the table department.
+CREATE TABLE department ( 
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
+);
+
+-- Create the table role.
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+);
+
+-- Create the table employee.
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
